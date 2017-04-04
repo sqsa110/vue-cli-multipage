3c0d538884688272b85c2e464611d8d92d5c5d94
sqsa110 <254750295@qq.com>
2017-04-04 23:08:46 +0800

    updata

--- src/module/login/login.js
+++ src/module/login/login.js
@@ -1,11 +1,54 @@
 import 'element-ui/lib/theme-default/index.css'
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
 import App from './app'
-Vue.use(ElementUI)
+
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+
+let router = new VueRouter([
+  {
+    path : '/foo',
+    component : App
+  },
+  {
+    path : "/bar",
+    component : App
+  }
+]);
+
+/*
+router.map({
+  '/view-a' : {
+    component : App
+  },
+  '/view-b' : {
+    component : App
+  }
+});
+*/
+new Vue({
+  router
+}).$mount('#app');
+
+/*
+let fd = {
+  '/view-a' : {
+    component : App
+  },
+  '/view-b' : {
+    component : App
+  }
+}
+
+//  router.start(fd,"#app");
+*/
 /* eslint-disable no-new */
+/*
 new Vue({
   el: '#app',
 //  components: { App }
   render: h => h(App)
 })
+*/
